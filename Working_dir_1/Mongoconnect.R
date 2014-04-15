19a4b758b165262ccbcbe48927aa8b1cf22bf2d2
arunrajagvk <arunrajagvk@hotmail.com>
2014-04-15 12:34:13 +0530

    Adding new R file for SQL and MongoConnector

---
+++ Working_dir_1/Mongoconnect.R
@@ -0,0 +1,60 @@
+install.packages("RODBC")
+library(RODBC)
+dbhandle <- odbcDriverConnect('driver={SQL Server};server=LT-AS5.lister.lan;database=VC_POC;trusted_connection=true')
+
+
+install.package
+R CMD INSTALL rJava
+install.packages("rJava",type='source')
+
+######################################################
+
+SQL SERVER CONNECTION
+
+######################################################
+require(RJDBC)
+drv <- JDBC("com.microsoft.sqlserver.jdbc.SQLServerDriver",
+            "/home/arunraja/Documents/sqljdbc4.jar") 
+conn <- dbConnect(drv, "jdbc:sqlserver://LT-AS5.lister.lan", "lister", "lister_123")
+#then build a query and run it
+args(dbConnect)
+showMethods("dbConnect") 
+
+sqlText <- paste(" use VC_POC;
+   SELECT top 10 * FROM t_Products_New_Local
+  ", sep=";")
+
+queryResults <- dbGetQuery(conn, sqlText)
+
+
+
+######################################################
+
+MONGO DB CONNECTION
+
+######################################################
+
+library("rmongodb")
+mongo <- mongo.create(host = "LT-AS5.lister.lan", name = "", username = "",
+                      password = "", db = "test", timeout = 0L)
+
+if (mongo.is.connected(mongo)) {
+  buf <- mongo.bson.buffer.create()
+  mongo.bson.buffer.append(buf, "name", "Joe")
+  mongo.bson.buffer.append(buf, "age", 22L)
+  b <- mongo.bson.from.buffer(buf)
+  mongo.insert(mongo, "test.people", b)
+}
+
+lst <- list(name="Arunraja", age=25)
+b <- mongo.bson.from.list(lst)
+# the above produces a BSON object of the form:
+# { "name" : "John", "age" : 32.0 }
+# Convert a vector of an atomic type to a list and
+# then to a mongo.bson object
+v <- c(president="Jefferson", vice="Burr")
+b <- mongo.bson.from.list(as.list(v))
+mongo.insert(mongo, "test.people", b)
+# the above produces a BSON object of the form:
+# { "president" : "Jefferson", "vice" : "Burr" }
+
